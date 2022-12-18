4ad3c1ec8a9614e14e33343613944b3e31468e74
Geoffrey Gao <geoffreygao1@gmail.com>
2022-12-19 03:24:47 +0900

    finished setting up initial folder structure

---
+++ __tests__/weekday.test.js
@@ -0,0 +1,12 @@
+import Date from './../src/weekday';
+
+describe('Date', () => {
+
+  // test('should correctly create a triangle object with three lengths', () => {
+  //   const triangle = new Triangle(2,4,5);
+  //   expect(triangle.side1).toEqual(2);
+  //   expect(triangle.side2).toEqual(4);
+  //   expect(triangle.side3).toEqual(5);
+  // });
+
+});
