d5b4e828d4550189e036aedb9662e855cc9832ba
Geoffrey Gao <geoffreygao1@gmail.com>
2022-12-19 03:34:01 +0900

    good fail on object constructor

--- __tests__/weekday.test.js
+++ __tests__/weekday.test.js
@@ -1,12 +1,15 @@
-import Date from './../src/weekday';
+import Dates from './../src/weekday.js';
 
-describe('Date', () => {
+describe('Dates', () => {
+  let testDate;
 
-  // test('should correctly create a triangle object with three lengths', () => {
-  //   const triangle = new Triangle(2,4,5);
-  //   expect(triangle.side1).toEqual(2);
-  //   expect(triangle.side2).toEqual(4);
-  //   expect(triangle.side3).toEqual(5);
-  // });
+  beforeEach(() => {
+    testDate = new Dates();
+  });
+
+  test('should create a Date object with a longDate and a weekday property', () => {
+    expect(testDate.longDate).toEqual('');
+    expect(testDate.weekday).toEqual('');
+  });
 
 });
