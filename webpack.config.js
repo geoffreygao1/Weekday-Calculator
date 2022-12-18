5edd065caf61edacdd2d5211b972cfec3a399ca8
Geoffrey Gao <geoffreygao1@gmail.com>
2022-12-19 03:21:56 +0900

    Added new dependencies for testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
+const ESLintPlugin = require('eslint-webpack-plugin');   // new line!
+
 
 module.exports = {
   entry: './src/index.js',
@@ -6,6 +10,19 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {                 // new line
+    contentBase: './dist'      // new line
+  },
+  devtool: 'eval-source-map',  // new line
+  plugins: [
+    new ESLintPlugin(),
+    new CleanWebpackPlugin(), // new line
+    new HtmlWebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
   module: {
     rules: [
       {
