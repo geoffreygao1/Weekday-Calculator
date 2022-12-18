fc66f31f8a6dd550ec4b953fc37f4d82f4083fcf
Geoffrey Gao <geoffreygao1@gmail.com>
2022-12-19 03:54:02 +0900

    initialized getDay function
    Co-authored-by: Mike Wilkes <mikewilkes@outlook.com>

--- src/weekday.js
+++ src/weekday.js
@@ -1,4 +1,12 @@
 export default function Dates() {
-  // this.longDate = "";
-  // this.weekday = "";
+  this.longDate = "";
+  this.weekday = "";
+}
+
+//HTML format: YYYY-MM-DD
+// run some function 
+// change this.weekday to "Monday" or something
+
+Dates.prototype.getDay = function () {
+
 }
