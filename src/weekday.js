0acec3d25f311b4a556f76764913db4e3597f08d
Geoffrey Gao <geoffreygao1@gmail.com>
2022-12-21 11:36:37 +0900

    Changed prototype objects to classes
    Co-authored-by: Mike Wilkes <mikewilkes@outlook.com>

--- src/weekday.js
+++ src/weekday.js
@@ -1,12 +1,37 @@
-export default function Dates() {
-  this.longDate = "";
-  this.weekday = "";
+// export default function Dates() {
+//   this.longDate = "";
+//   this.weekday = "";
+// }
+
+// // YYYY-MM-DD
+
+// Dates.prototype.getDay = function () {
+//   const date1 = new Date(this.longDate);
+//   const dateArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//   this.weekday = dateArray[date1.getDay()];
+// };
+
+export default class Dates {
+
+  constructor() {
+    this.longDate = "";
+    this.weekday = "";
+  }
+
+  getDay() {
+    const date1 = new Date(this.longDate);
+    const dateArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    this.weekday = dateArray[date1.getDay()];
+  }
 }
 
-// YYYY-MM-DD
+// class Triangle {
 
-Dates.prototype.getDay = function () {
-  const date1 = new Date(this.longDate);
-  const dateArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  this.weekday = dateArray[date1.getDay()];
-};
+//   constructor() {
+//     this.variableScopedToConstructor = 0;
+//   }
+
+//   checkType() {
+//     let variableScopedToMethod = 0;
+//   }    
+// }
